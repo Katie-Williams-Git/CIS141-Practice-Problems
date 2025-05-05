9716fec59bda63ed64c5cd66b9a559b5abf5aac2
Katie Williams <katiewilliams@student.olympic.edu>
2025-05-04 17:18:15 -0700

    Create practice3.py

---
+++ Mod4/practice3.py
@@ -0,0 +1,8 @@
+'''3. Prompt the user for their bank balance. Evaluate whether the balance is less than $100. 
+Print True if the user’s balance is below $100; print False, otherwise.
+'''
+bal = int(input("What is your bank account balance? "))
+if bal > 100:
+    print("False")
+else: 
+    print("True")
