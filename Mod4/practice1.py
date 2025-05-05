908d9d45c3ae375150705a1fb552377b05e215ff
Katie Williams <katiewilliams@student.olympic.edu>
2025-05-04 17:17:17 -0700

    Create practice1.py

---
+++ Mod4/practice1.py
@@ -0,0 +1,8 @@
+'''1. Construct a truth table for the expression: (A AND B) OR (NOT B) where A and B each can be True or False. 
+Your truth table should be commented out (as it's not valid Python code!)
+'''
+#A B  (A AND B) OR (NOT B)
+#T T   T             T
+#T F   F             T
+#F T   F             F
+#F F   T             F
