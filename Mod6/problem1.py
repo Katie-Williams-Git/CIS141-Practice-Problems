465ebe1aaa852d2a6b48f182a3b6cbdae2ce2a9c
Katie Williams <katiewilliams@student.olympic.edu>
2025-05-19 18:12:01 -0700

    Create problem1.py
    one of many solutions

---
+++ Mod6/problem1.py
@@ -0,0 +1,6 @@
+#1. Create a list of integers (you get to pick!). Write code to iterate through the list
+#and calculate the sum of all even numbers. Print the resulting sum.
+list = [5, 10, 15, 20, 25, 30, 35, 40]
+alleven = list[1:8:2]
+sumalleven = sum(alleven)
+print(sumalleven)
