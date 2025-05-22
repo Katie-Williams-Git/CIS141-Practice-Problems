7a67010bf7bbaa0f1f6dc185b284ccdfaacfbcf4
Katie Williams <katiewilliams@student.olympic.edu>
2025-05-22 14:55:21 -0700

    Update problem1.py
    added additional solution

--- Mod6/problem1.py
+++ Mod6/problem1.py
@@ -4,3 +4,12 @@ list = [5, 10, 15, 20, 25, 30, 35, 40]
 alleven = list[1:8:2]
 sumalleven = sum(alleven)
 print(sumalleven)
+
+#I came up with a different solution that would be more accurate for any numbers added to the list
+
+list = [5, 10, 15, 20, 25, 30, 35, 40]
+finalsum = 0
+for iseven in list:
+    if iseven % 2 == 0:
+        finalsum += iseven
+print(finalsum)
