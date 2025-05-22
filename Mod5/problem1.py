e34f6ed0d827c728df00c808bb73cfae19ed442e
Katie Williams <katiewilliams@student.olympic.edu>
2025-05-22 13:02:33 -0700

    Update problem1.py
    Added a solution to the problem instead of the code posted before that solves a different problem

--- Mod5/problem1.py
+++ Mod5/problem1.py
@@ -1,6 +1,6 @@
 #1. Prompt the user for a positive integer n. Use a while loop to sum all the integers from 1 up to n. 
 #Print the final sum.
-
+'''
 n = int(input("Please give me a positive whole number. "))
 start = 1
 while start <= n:
@@ -8,4 +8,15 @@ while start <= n:
     start+=1
     if start == n + 1:
         break
-    
+#This code doesn't solve the problem but I have kept it in purely for the record.
+'''
+#1. Prompt the user for a positive integer n. Use a while loop to sum all the integers from 1 up to n.
+#Print the final sum.
+
+n = int(input("Please give me a positive whole number. "))
+start = 0
+finalsum = 0
+while start < n:
+    start += 1
+    finalsum += start
+print(finalsum)
